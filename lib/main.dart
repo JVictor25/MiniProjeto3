2932adca4148297f5fa6ab9244153b3ac9e16d96
joaovictor <joaovictorguedes2003@hotmail.com>
2024-11-17 22:09:55 -0300

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -41,9 +41,3 @@ class MeuApp extends StatelessWidget {
     );
   }
 }
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/JVictor25/MiniProjeto3.git
-git push -u origin main
